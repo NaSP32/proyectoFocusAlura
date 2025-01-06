6786b531630e0dde5ac644edcf0f9359b413a871
Nadia Soledad Pereyra <nadiapereyra32@gmail.com>
2025-01-06 11:18:34 -0300

    Modifico los textos en pantalla de acuerdo al boton de descanso seleccionado utilizando inner.HTML desde js

--- proyecto-base/script.js
+++ proyecto-base/script.js
@@ -2,7 +2,8 @@ const html = document.querySelector('html');
 const botonEnfoque = document.querySelector('.app__card-button--enfoque');
 const botonCorto = document.querySelector('.app__card-button--corto');
 const botonLargo = document.querySelector('.app__card-button--largo');
-const banner = document.querySelector('.app__image')
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
 
 botonEnfoque.addEventListener('click', () => {
     cambiarContexto('enfoque')
@@ -27,7 +28,28 @@ botonLargo.addEventListener('click', () => {
 
 function cambiarContexto(contexto){
     html.setAttribute('data-contexto', contexto);
-    banner.setAttribute('src',  `./imagenes/${contexto}.png`)
+    banner.setAttribute('src',  `./imagenes/${contexto}.png`);
+    switch (contexto) {
+        case "enfoque":
+            titulo.innerHTML = `Optimiza tu productividad,<br>
+                <strong class="app__title-strong">sumérgete en lo que importa.</strong>
+            `            
+            break;
+        case "descanso-corto":
+            titulo.innerHTML = `¿Qué tal tomar un respiro? 
+                <strong class="app__title-strong">¡Haz una pausa corta!</strong>
+                `
+            break;
+        case "descanso-largo":
+            titulo.innerHTML = `Hora de volver a la superficie 
+            <strong class="app__title-strong">Haz una pausa larga.</strong>
+            `
+            break;
+            
+    
+        default:
+            break;
+    }
 
 
 }
