3183a8e16ada4e94a4c44b00d20d2e4c4fb94e02
Nadia Soledad Pereyra <nadiapereyra32@gmail.com>
2025-01-06 01:57:43 -0300

    Utilizo setAttribute para modificar un elemento por otro y asi darle funcionalidad a los botones de descanso. Se crea una funcion para optimizar el codigo inicial

--- proyecto-base/script.js
+++ proyecto-base/script.js
@@ -2,18 +2,32 @@ const html = document.querySelector('html');
 const botonEnfoque = document.querySelector('.app__card-button--enfoque');
 const botonCorto = document.querySelector('.app__card-button--corto');
 const botonLargo = document.querySelector('.app__card-button--largo');
+const banner = document.querySelector('.app__image')
 
 botonEnfoque.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'enfoque');
+    cambiarContexto('enfoque')
 })
 
 botonCorto.addEventListener('click', () => {
-    /*setAtributte agarra un valor y lo sustituye por otro*/
+    /*setAtributte agarra un valor y lo sustituye por otro
     html.setAttribute('data-contexto', 'descanso-corto' );
+    banner.setAttribute('src', './imagenes/descanso-corto.png')*/
+    /*optimizo con una function*/
+    cambiarContexto('descanso-corto')
+
 })
 
 
 botonLargo.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-largo');
+    /*html.setAttribute('data-contexto', 'descanso-largo');
+    banner.setAttribute('src', './imagenes/descanso-largo.png')*/
+    cambiarContexto('descanso-largo')
 
 })
+
+function cambiarContexto(contexto){
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src',  `./imagenes/${contexto}.png`)
+
+
+}
